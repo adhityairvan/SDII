72c7a1aeb07ca112d5d683d6a463f5ddb32f42ff
adhityairvan <adhityairvan@yahoo.com>
2016-10-04 19:48:33 -0700

    upload pertama

--- Bubblesort.c
+++ Bubblesort.c
@@ -1,6 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
-#define SIZE 10
+
 int tukar(int a[],int b[]){
 	int t;
 	t=*a;
@@ -8,36 +8,34 @@ int tukar(int a[],int b[]){
 	*b=t;
 }
 
-int urut(int data[]){
+int urut(int data[],int top){
 	int i,j;
-	for(i=0;i<SIZE;i++){
-		for(j=0;j<SIZE-i;j++){
+	for(i=0;i<top-1;i++){
+		for(j=0;j<top-1;j++){
 			if(data[j]>data[j+1])tukar(&data[j],&data[j+1]);
 		}
 	}
 }
-void cetak(int data[]){
+void cetak(int data[],int top){
 	int i;
-	for(i=0;i<SIZE;i++){
+	for(i=0;i<top;i++){
 		printf("%d\n",data[i]);
 	}
 }
 
 int main(){
-	int i;
-	int array[SIZE];
-	printf("Masukkan Data 1-10\n");
-	for(i=0;i<SIZE;i++){
-		scanf("%d",&array[i]);
-	}
+	int i=0;
+	int array[1000];
+	printf("Masukkan Data..\n");
+	while(scanf("%d",&array[i])!=EOF)i++;
 	char pilihan;
 	printf("Urutkan?y/n\n");
 	scanf(" %c",&pilihan);
 	if(pilihan=='y'){
-		urut(array);
-		cetak(array);
+		urut(array,i);
+		cetak(array,i);
 	}
 	else{
-		cetak(array);
+		cetak(array,i);
 	}
 }
